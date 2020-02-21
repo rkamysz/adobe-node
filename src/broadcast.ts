7998c2ed29463a1fdb8441e483c0160ff8abb56d
Christian Oeing <christian.oeing@slashgames.org>
2020-02-21 18:51:55 +0100

    Add result and error to msg

--- src/broadcast.ts
+++ src/broadcast.ts
@@ -4,12 +4,12 @@ import { exec } from 'child_process';
 const broadcastMethods: Map<AdobeAppName, (cmd: string, payload: string) => string> = new Map<AdobeAppName, (cmd: string, payload: string) => string>([
   [AdobeAppName.Animate, (cmd: string, payload: string) => `FLfile.runCommandLine("${cmd}");`],
   [AdobeAppName.Photoshop, (cmd: string, payload: string) => `app.system("${cmd}");`],
-  [AdobeAppName.Illustrator, (cmd: string, payload: string) => `
+  [AdobeAppName.Illustrator, (cmd: string, command: string) => `
   const bt = new BridgeTalk();
   bt.target = 'bridge';
   const btCmd = '${cmd.replace(/'/g, "\\'")}';
   bt.body = 'const cmd = "' + btCmd + '";';
-  bt.body += 'const msg = \\\'${payload}\\\';';
+  bt.body += 'const msg = \\\'{"command": "${command}", "stdout": "' + __stdout + '", "stderr": "' + __stderr + '"}\\\';';
   bt.body += 'const sys = cmd + " --msg=\\\'" + msg + "\\\'";';
   bt.body += 'app.system(sys);';
   bt.onError = function(bt)  
@@ -21,22 +21,21 @@ const broadcastMethods: Map<AdobeAppName, (cmd: string, payload: string) => stri
   `]
 ]);
 
-const buildBroadcastCommand = (host: string, port: number, message: string) =>
+const buildBroadcastCommand = (host: string, port: number) =>
   `adobe-broadcast --host='${host}' --port=${port}`;
 
 export const newBroadcastBuilder = (config: Config): BroadcastBuilder => {
 
   return {
     build(command: string) {
-      const payload: string = `{"command": "${command}"}`;
       const broadcast: (msg: string, payload: string) => string = broadcastMethods.get(config.app.name);
-      const cmd: string = buildBroadcastCommand(config.host, config.port, payload);
-      return broadcast(cmd, payload);
+      const cmd: string = buildBroadcastCommand(config.host, config.port);
+      return broadcast(cmd, command);
     }
   }
 }
 
 export const broadcast = (host: string, port: number, message: BroadcastMessage) => {
-  const cmd: string = buildBroadcastCommand(host, port, JSON.stringify(message));
+  const cmd: string = buildBroadcastCommand(host, port);
   exec(cmd);
 }
