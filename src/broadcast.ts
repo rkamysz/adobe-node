44d26e222d6ac1186cee623839213fe16a143b31
Christian Oeing <christian.oeing@slashgames.org>
2020-02-26 14:57:48 +0100

    Revert changes to broadcast logic of Photoshop and Animate to make them work again

--- src/broadcast.ts
+++ src/broadcast.ts
@@ -1,10 +1,20 @@
 import { AdobeAppName, BroadcastBuilder, BroadcastMessage, Config } from "./api";
 import { exec } from 'child_process';
 
-const broadcastMethods: Map<AdobeAppName, (cmd: string, payload: string) => string> = new Map<AdobeAppName, (cmd: string, payload: string) => string>([
-  [AdobeAppName.Animate, (cmd: string, payload: string) => `FLfile.runCommandLine("${cmd}");`],
-  [AdobeAppName.Photoshop, (cmd: string, payload: string) => `app.system("${cmd}");`],
-  [AdobeAppName.Illustrator, (cmd: string, command: string) => `
+const createPayload = (command: string): string => {
+  return `{\\"command\\":\\"${command}\\",\\"stdout\\":\\"" + __stdout + "\\", \\"stderr\\":\\"" + __stderr + "\\" }`;
+};
+
+const buildBroadcastCommand = (host: string, port: number, message: string): string => {
+  return `adobe-broadcast --host='${host}' --port=${port} --msg='${message}'`;
+}
+
+const broadcastMethods: Map<AdobeAppName, (host: string, port: number, command: string) => string> = new Map<AdobeAppName, (host: string, port: number, command: string) => string>([
+  [AdobeAppName.Animate, (host: string, port: number, command: string) => `FLfile.runCommandLine("${buildBroadcastCommand(host, port, createPayload(command))}");`],
+  [AdobeAppName.Photoshop, (host: string, port: number, command: string) => `app.system("${buildBroadcastCommand(host, port, createPayload(command))}");`],
+  [AdobeAppName.Illustrator, (host: string, port: number, command: string) => {
+    const cmd = `adobe-broadcast --host='${host}' --port=${port}`;
+    return `
   if (!__stdout) {
     __stdout = "{}";
   }
@@ -23,24 +33,22 @@ const broadcastMethods: Map<AdobeAppName, (cmd: string, payload: string) => stri
     alert("Error from BridgeTalk: " + bt.body); 
   };
   bt.send();
-  `]
+  `
+  }
+  ]
 ]);
-
-const buildBroadcastCommand = (host: string, port: number) =>
-  `adobe-broadcast --host='${host}' --port=${port}`;
 
 export const newBroadcastBuilder = (config: Config): BroadcastBuilder => {
 
   return {
     build(command: string) {
-      const broadcast: (msg: string, payload: string) => string = broadcastMethods.get(config.app.name);
-      const cmd: string = buildBroadcastCommand(config.host, config.port);
-      return broadcast(cmd, command);
+      const broadcast = broadcastMethods.get(config.app.name);
+      return broadcast(config.host, config.port, command);
     }
   }
 }
 
 export const broadcast = (host: string, port: number, message: BroadcastMessage) => {
-  const cmd: string = buildBroadcastCommand(host, port);
+  const cmd: string = buildBroadcastCommand(host, port, JSON.stringify(message));
   exec(cmd);
 }
